9dd37f4eb10320beaadb6c087b402dccb564c97b
3Dcodety <202508130435@csust.edu.cn>
2026-09-24 00:17:29 +0800

    feat:完成todo页面增删待办功能

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # my-todo-vue-admin
-A full-stack todo management system,learning vue-admin development step by step.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" style="padding:30px;">
+    <!-- 输入框 + 添加按钮 -->
+    <el-input v-model="todoText" placeholder="输入待办事项" style="width:300px;margin-right:10px;"></el-input>
+    <el-button type="primary" @click="addTodo">添加</el-button>
+
+
+    <!--  待办表格 -->
+    <el-table :data="todoList" border style="width:500px;margin-top:20px;">
+      <el-table-column prop="content" label="待办内容"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="danger" size="mini" @click="delTodo(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
